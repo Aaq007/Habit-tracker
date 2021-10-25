027e75c61bbf335c903c1a1f1a0af79ba141973f
Aaqil Shihab <aaqilsh@yahoo.com>
2021-10-25 12:34:54 +0530

    Added models folder

---
+++ lib/models/task_items.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+List<Card> items = [
+  Card(
+    child: ListTile(
+      title: const Text('Task'),
+      onTap: () {},
+    ),
+  ),
+  Card(
+    child: ListTile(
+      title: Text('Task'),
+    ),
+  ),
+  Card(
+    child: ListTile(
+      title: Text('Task'),
+    ),
+  ),
+  Card(
+    child: ListTile(
+      title: Text('Task'),
+    ),
+  ),
+];
