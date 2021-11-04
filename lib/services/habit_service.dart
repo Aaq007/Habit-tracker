7120840d16cb8647cf8353f76d9006cdd5becd31
Aaqil Shihab <aaqilsh@yahoo.com>
2021-11-04 23:41:59 +0530

    Added test endpoint for testing purpose

--- lib/services/habit_service.dart
+++ lib/services/habit_service.dart
@@ -1,7 +1,8 @@
 import 'package:http/http.dart' as http;
 
 void getHabits() async {
-  var url = Uri.parse('http://10.0.2.2:8000/habits/');
+  // var url = Uri.parse('http://10.0.2.2:8000/habits/');
+  var url = Uri.parse('https://reqres.in/api/users?page=2');
   var response = await http.get(url);
   print(response.body);
 }
