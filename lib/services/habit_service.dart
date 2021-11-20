4e1bfdf30aec9106304212c939251df328a0d690
Aaqil Shihab <aaqilsh@yahoo.com>
2021-11-20 19:46:08 +0530

    Modified getHabits

--- lib/services/habit_service.dart
+++ lib/services/habit_service.dart
@@ -2,10 +2,11 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-void getHabits() async {
+Future<Map<String, dynamic>> getHabits() async {
   // var url = Uri.parse('http://10.0.2.2:8000/habits/');
   var url = Uri.parse('https://reqres.in/api/users?page=2');
   var response = await http.get(url);
   var decoded = jsonDecode(response.body);
+  return decoded;
   // print(response.body);
 }
