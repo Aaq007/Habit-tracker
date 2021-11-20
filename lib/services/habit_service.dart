227ec09da0c2affda9d4b7594a3e9d8a53caa353
Aaqil Shihab <aaqilsh@yahoo.com>
2021-11-20 19:38:43 +0530

    Tested network service

--- lib/services/habit_service.dart
+++ lib/services/habit_service.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 void getHabits() async {
   // var url = Uri.parse('http://10.0.2.2:8000/habits/');
   var url = Uri.parse('https://reqres.in/api/users?page=2');
   var response = await http.get(url);
-  print(response.body);
+  var decoded = jsonDecode(response.body);
+  print(decoded['data'].length);
+  // print(response.body);
 }
